a941d8f5bc1b9c0eb76a09e382a2eab2f3ead0f8
DAVID SHIN <kilcmn3@hotmail.com>
2019-12-15 13:01:41 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,9 +1,15 @@
 # code your #position_taken? method here!
 
 def position_taken?(board, index)
+<<<<<<< HEAD
   if board[index] == " " || board[index] == "" || board[index] == nil
     return false
   else
     return true
+=======
+  if board[index] === " " || "" ||
+    return false
+
+>>>>>>> beb1985e8db067e18c98b8ea7e8d16f1e4b9a5ff
   end
 end
